a7a35c78cbf93bf35165271c63d39424bd5b7155
Boburxon83 <137619319+Boburxon83@users.noreply.github.com>
2023-07-08 17:52:44 +0500

    Добавьте файлы проекта.

---
+++ HomeWork03/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HomeWork03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int userInput;
+            int count = 1;
+            Random rand = new Random();
+
+            number = rand.Next(1, 100);
+
+            Console.WriteLine("1 dan 100 gacha bo'lgan sonni toping, bu qaysi son = ");
+
+            while (true)
+            {
+                Console.WriteLine("Sizning variantingiz = ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == number)
+                {
+                    Console.WriteLine("Siz variantingiz to'g'ri bu " + number + " soni. Siz " + count + " - chi urinishda topdingiz.");
+                    break;
+                }
+                if (userInput > number)
+                {
+                    Console.WriteLine($"Sizning variantingizdagi son {userInput} dan kichik:");
+                }
+                else
+                {
+                    Console.WriteLine($"Sizning variantingizdagi son {userInput} dan katta:");
+                }
+                count++;
+            }
+            Console.ReadLine();
+        }
+    }
+ }
